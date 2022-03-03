f4a4b85fbbe23b08187a134c9d12219c16e20801
DoceAzedo <oi@lucasfernandes.com.br>
2022-03-03 00:44:01 -0300

    👷 Fix build

--- scripts/package.js
+++
@@ -1,8 +0,0 @@
-const { copyFileSync } = require('fs');
-const path = require('path');
-
-const main = () => copyFileSync(
-  path.resolve(__dirname, '../package.json'),
-  path.resolve(__dirname, '../dist/package.json')
-);
-main();
